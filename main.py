d329f4053f01928870e0478d31efa8a2b4f0ff6e
一头大白兔 <kyle.script@gmail.com>
2018-05-04 20:51:55 +0800

    添加 utils.py 文件

--- main.py
+++ main.py
@@ -1,24 +1,51 @@
 # coding=utf-8
 
 import sys
+from functools import partial
 
 from PyQt5 import QtCore
 from PyQt5 import QtWidgets
 from PyQt5.QtGui import QIcon
+
 import main_ui
 import set_ui
+from utils import get_config, save_config
+
+main_Dialog = None
+ak = None
+sk = None
+
+
+def save_key_set_dialog(sui, set_dialog):
+    global ak, sk
+
+    edit_ak = sui.lineEdit_ak.text()
+    edit_sk = sui.lineEdit_sk.text()
+    if edit_ak == "" or edit_sk == "":
+        QtWidgets.QMessageBox.information(main_Dialog, '警告', "AccessKey和SecretKey都不能为空哦！")
+        return
+
+    save_config(edit_ak, edit_sk)
+    ak = edit_ak
+    sk = edit_sk
+    set_dialog.close()
 
 
 def show_set_dialog():
-    set_Dialog = QtWidgets.QDialog(main_Dialog, flags=QtCore.Qt.WindowCloseButtonHint)
+    set_dialog = QtWidgets.QDialog(main_Dialog, flags=QtCore.Qt.WindowCloseButtonHint)
     sui = set_ui.Ui_Dialog()
-    sui.setupUi(set_Dialog)
-    set_Dialog.show()
+    sui.setupUi(set_dialog)
+    sui.pushButton.clicked.connect(partial(save_key_set_dialog, sui, set_dialog))
+    set_dialog.show()
 
 
 def init():
+    global ak, sk
+
     # 检查AccessKey和SecretKey
-    pass
+    ak, sk = get_config()
+    if ak is None:
+        show_set_dialog()
 
 
 if __name__ == "__main__":
---
+++ utils.py
@@ -0,0 +1,22 @@
+# coding=utf-8
+
+import json
+
+CONFIG_FILE = 'config.json'
+
+
+def save_config(ak, sk):
+    with open(CONFIG_FILE, "w") as f:
+        f.write('{"AccessKey":"%s", "SecretKey":"%s"}' % (ak, sk))
+
+
+def get_config():
+    try:
+        with open(CONFIG_FILE, "r") as f:
+            json_str = f.read()
+            json_dict = json.JSONDecoder().decode(json_str)
+            if 'AccessKey' in json_dict and 'SecretKey' in json_dict:
+                return json_dict['AccessKey'], json_dict['SecretKey']
+    except Exception as e:
+        print("utils.py get_config() failed:" + str(e))
+    return None, None
