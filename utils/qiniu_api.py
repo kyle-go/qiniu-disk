2bae79a015ca30accfa0dda8001338a30c65585b
一头大白兔 <kyle.script@gmail.com>
2018-05-04 23:55:57 +0800

    获取指定空间域名列表

--- utils/qiniu_api.py
+++ utils/qiniu_api.py
@@ -6,10 +6,30 @@ import requests
 from qiniu import Auth
 
 
+# 获取空间列表
 def get_buckets(ak, sk):
     try:
         authorization = Auth(ak, sk).token_of_request("/buckets")
         url = "http://rs.qbox.me/buckets"
+        headers = {
+            'Authorization': "QBox " + authorization,
+            'User-Agent': 'client by "https://gitee.com/kylescript/qiniu-disk"',
+            'Accept-Encoding': 'gzip'
+        }
+        response = requests.request("GET", url, headers=headers)
+        if response.status_code == 200:
+            return json.JSONDecoder().decode(response.text)
+        print("qiniu_api.py get_buckets() failed:" + response.text)
+    except Exception as e:
+        print("qiniu_api.py get_buckets() failed:" + str(e))
+    return ()
+
+
+# 获取指定空间域名列表
+def get_bucket_domains(ak, sk, bucket):
+    try:
+        authorization = Auth(ak, sk).token_of_request("/v6/domain/list?tbl=%s" % bucket)
+        url = "http://api.qiniu.com/v6/domain/list?tbl=%s" % bucket
         headers = {
             'Authorization': "QBox " + authorization,
             'User-Agent': 'client by "https://gitee.com/kylescript/qiniu-disk"',
