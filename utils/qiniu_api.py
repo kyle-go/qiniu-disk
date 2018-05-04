6c0f4288f2e5a95c29e9e00a0584a325b51bd388
一头大白兔 <kyle.script@gmail.com>
2018-05-04 21:49:05 +0800

    update get_buckets

--- utils/qiniu_api.py
+++ utils/qiniu_api.py
@@ -7,14 +7,18 @@ from qiniu import Auth
 
 
 def get_buckets(ak, sk):
-    authorization = Auth(ak, sk).token_of_request("/buckets")
-    url = "http://rs.qbox.me/buckets"
-    headers = {
-        'Authorization': "QBox " + authorization,
-        'User-Agent': 'client by "https://gitee.com/kylescript/qiniu-disk"',
-        'Accept-Encoding': 'gzip'
-    }
-    response = requests.request("GET", url, headers=headers)
-    if response.status_code == 200:
-        return json.JSONDecoder().decode(response.text)
+    try:
+        authorization = Auth(ak, sk).token_of_request("/buckets")
+        url = "http://rs.qbox.me/buckets"
+        headers = {
+            'Authorization': "QBox " + authorization,
+            'User-Agent': 'client by "https://gitee.com/kylescript/qiniu-disk"',
+            'Accept-Encoding': 'gzip'
+        }
+        response = requests.request("GET", url, headers=headers)
+        if response.status_code == 200:
+            return json.JSONDecoder().decode(response.text)
+        print("qiniu_api.py get_buckets() failed:" + response.text)
+    except Exception as e:
+        print("qiniu_api.py get_buckets() failed:" + str(e))
     return ()
