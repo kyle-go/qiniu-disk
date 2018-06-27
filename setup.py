5c048d276332e8fb9bd1553ec9cb46a0a77bcfda
一头大白兔 <kyle.script@gmail.com>
2018-06-27 12:45:27 +0800

    add cx_freeze build

--- setup.py
+++ setup.py
@@ -4,4 +4,6 @@ from cx_Freeze import setup, Executable
 setup(name = "main" ,
       version = "0.1" ,
       description = "" ,
-      executables = [Executable("main.py")])
+      executables = [Executable("main.py")],
+      options={"build_exe": {"packages": ["multiprocessing", 'idna']}}
+)
