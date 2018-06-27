e2efd5919aa5ea17c25bd2a059c0544e0f62c1c9
一头大白兔 <kyle.script@gmail.com>
2018-06-27 10:46:26 +0800

    add cx_freeze build

---
+++ setup.py
@@ -0,0 +1,7 @@
+# -*- coding: utf-8 -*-
+from cx_Freeze import setup, Executable
+
+setup(name = "main" ,
+      version = "0.1" ,
+      description = "" ,
+      executables = [Executable("main.py")])
