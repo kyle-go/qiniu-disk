767b08f6e577166f8d48c30ca0bf340e380c953b
一头大白兔 <kyle.script@gmail.com>
2018-06-27 12:59:45 +0800

    build win32Gui

--- setup.py
+++ setup.py
@@ -1,9 +1,14 @@
 # -*- coding: utf-8 -*-
+import sys
 from cx_Freeze import setup, Executable
 
-setup(name = "main" ,
-      version = "0.1" ,
-      description = "" ,
-      executables = [Executable("main.py")],
+base = ""
+if sys.platform == 'win32':
+    base = 'Win32GUI'
+
+setup(name="main",
+      version="0.1",
+      description="",
+      executables=[Executable("main.py", base=base)],
       options={"build_exe": {"packages": ["multiprocessing", 'idna']}}
-)
+      )
