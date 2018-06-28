078e2489966ceb6088ff10b743537976aa99601b
一头大白兔 <kyle.script@gmail.com>
2018-06-28 13:33:05 +0800

    编写 delete_bucket_file 方法

--- utils/qiniu_api.py
+++ utils/qiniu_api.py
@@ -5,7 +5,7 @@
 import json
 import base64
 import requests
-from qiniu import Auth
+from qiniu import Auth, urlsafe_base64_encode
 
 # 空间区域
 Region = ['z0', 'z1', 'z2', 'na0', 'as0']
@@ -90,4 +90,25 @@ def get_bucket_files(ak, sk, bucket, marker, limit, prefix, delimiter="/"):
         print("qiniu_api.py get_bucket_files() failed:" + response.text)
     except Exception as e:
         print("qiniu_api.py get_bucket_files() failed:" + str(e))
+    return False, None
+
+
+# 删除文件
+def delete_bucket_file(ak, sk, bucket, name):
+    try:
+        sub_url = "/delete/%s" % urlsafe_base64_encode(bucket + ":" + name)
+        authorization = Auth(ak, sk).token_of_request(sub_url)
+        url = "http://rs.qiniu.com%s" % sub_url
+        headers = {
+            'Authorization': "QBox " + authorization,
+            'User-Agent': 'client by "https://gitee.com/kylescript/qiniu-disk"',
+            'Accept-Encoding': 'gzip'
+        }
+        response = requests.request("POST", url, headers=headers)
+        if response.status_code == 200:
+            return True, 0
+        print("qiniu_api.py delete_bucket_file() failed:" + response.text)
+        return False, response.status_code
+    except Exception as e:
+        print("qiniu_api.py delete_bucket_file() failed:" + str(e))
     return False, None
